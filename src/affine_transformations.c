9ac1f6cd6092063e19dbca389ea34354db288a6b
neomamoo <you@example.com>
2024-10-01 04:06:27 +0700

    affin function version 2 (corrected)

--- src/affine_transformations.c
+++ src/affine_transformations.c
@@ -19,68 +19,59 @@ void s21_scale(Model *model, double value) {
 
 }
 
+// а что будет если попробую повернуть не загрузив модель
+// проверку return'ом???
+// будет ли ситуация нулевого value???
 void s21_rotate_forward(Model *model, double value, Axis_t axis) {
 
     value *= M_PI / 180;
 
-    if(axis == X) {
+    for (size_t i = 3; i < (model->total_vertices + 1) * 3; i += 3) {
 
-        for (size_t i = 3; i < (model->total_vertices + 1) * 3; i += 3) {
-            model->vertices[i + 1] = model->vertices[i + 1] * cos(value) + model->vertices[i + 2] * sin(value);
-            model->vertices[i + 2] = model->vertices[i + 1] * -sin(value) + model->vertices[i + 2] * cos(value);
+        double x = model->vertices[i];
+        double y = model->vertices[i + 1];
+        double z = model->vertices[i + 2];
+
+        if(axis == X) {
+            model->vertices[i + 1] = y * cos(value) + z * sin(value);
+            model->vertices[i + 2] = y * -sin(value) + z * cos(value);
         }
 
-    }
-
-    if(axis == Y) {
-
-        for (size_t i = 3; i < (model->total_vertices + 1) * 3; i += 3) {
-            model->vertices[i] = model->vertices[i] * cos(value) + model->vertices[i + 2] * -sin(value);
-            model->vertices[i + 2] = model->vertices[i] * sin(value) + model->vertices[i + 2] * cos(value);
+        if(axis == Y) {
+            model->vertices[i] = x * cos(value) + z * -sin(value);
+            model->vertices[i + 2] = x * sin(value) + z * cos(value);
         }
 
-    }
-
-    if(axis == Z) {
-
-        for (size_t i = 3; i < (model->total_vertices + 1) * 3; i += 3) {
-            model->vertices[i] = model->vertices[i] * cos(value) + model->vertices[i + 1] * sin(value);
-            model->vertices[i + 1] =  model->vertices[i] * -sin(value) + model->vertices[i + 1] * cos(value);
+        if(axis == Z) {
+            model->vertices[i] = x * cos(value) + y * sin(value);
+            model->vertices[i + 1] =  x * -sin(value) + y * cos(value);
         }
-
     }
-
-}
+} // s21_rotate_forward
 
 void s21_rotate_back(Model *model, double value, Axis_t axis) {
 
     value *= M_PI / 180;
 
-    if(axis == X) {
+    for (size_t i = 3; i < (model->total_vertices + 1) * 3; i += 3) {
 
-        for (size_t i = 3; i < (model->total_vertices + 1) * 3; i += 3) {
-            model->vertices[i + 1] = model->vertices[i + 1] * cos(value) + model->vertices[i + 2] * -sin(value);
-            model->vertices[i + 2] = model->vertices[i + 1] * sin(value) + model->vertices[i + 2] * cos(value);
+        double x = model->vertices[i];
+        double y = model->vertices[i + 1];
+        double z = model->vertices[i + 2];
+
+        if(axis == X) {
+            model->vertices[i + 1] = y * cos(value) + z * -sin(value);
+            model->vertices[i + 2] = y * sin(value) + z * cos(value);
         }
 
-    }
-
-    if(axis == Y) {
-
-        for (size_t i = 3; i < (model->total_vertices + 1) * 3; i += 3) {
-            model->vertices[i] = model->vertices[i] * cos(value) + model->vertices[i + 2] * sin(value);
-            model->vertices[i + 2] = model->vertices[i] * -sin(value) + model->vertices[i + 2] * cos(value);
+        if(axis == Y) {
+            model->vertices[i] = x * cos(value) + z * sin(value);
+            model->vertices[i + 2] = x * -sin(value) + z * cos(value);
         }
 
-    }
-
-    if(axis == Z) {
-
-        for (size_t i = 3; i < (model->total_vertices + 1) * 3; i += 3) {
-            model->vertices[i] = model->vertices[i] * cos(value) + model->vertices[i + 1] * -sin(value);
-            model->vertices[i + 1] =  model->vertices[i] * sin(value) + model->vertices[i + 1] * cos(value);
+        if(axis == Z) {
+            model->vertices[i] = x * cos(value) + y * -sin(value);
+            model->vertices[i + 1] =  x * sin(value) + y * cos(value);
         }
-
     }
-
-}
+} // s21_rotate_back
